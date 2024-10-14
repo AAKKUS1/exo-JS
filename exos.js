705126f5c3175fd8149db53ce9a5a35d575ef260
AAKKUS1 <aakkus@edenschool.fr>
2024-10-14 14:23:32 +0200

    feat(app)first-commit

---
+++ exos.js
@@ -0,0 +1,17 @@
+//exercice1
+
+//let degreeCelsius =prompt("entrer température en Celsius");
+//let degreeFarheneit =degreeCelsius*9/5+32;
+//console.log(degreeFarheneit)
+
+//exercice2
+//let a =Number(prompt("Entrer valeur a"));
+//let b =Number(prompt("Entrer valeur b"));
+//let c =Number(prompt("Entrer valeur c"));
+//let SommeTroisNombres = a+b+c;
+//console.log(SommeTroisNombres)
+
+//exercices3
+//let heuresEnSecondes =prompt("entrer nombre d'heures");
+//let Secondes =heuresEnSecondes*3600;
+//console.log(Secondes);
