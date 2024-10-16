02605d285355145de12ec84f339a7c261aef3b13
AAKKUS1 <aakkus@edenschool.fr>
2024-10-16 10:38:06 +0200

    chore(app)js-exercices

--- exos.js
+++ exos.js
@@ -1,5 +1,4 @@
 //exercice1
-
 //let degreeCelsius =prompt("entrer température en Celsius");
 //let degreeFarheneit =degreeCelsius*9/5+32;
 //console.log(degreeFarheneit)
@@ -13,5 +12,28 @@
 
 //exercices3
 //let heuresEnSecondes =prompt("entrer nombre d'heures");
-//let Secondes =heuresEnSecondes*3600;
-//console.log(Secondes);
+//let Secondes =heuresEnSecondes*36ondes);00;
+//console.log(Sec
+//16/10/24
+
+//exercice1
+//let age =Number(prompt("entrez votre âge"));
+//let nation =prompt("nationalité");
+
+//if (age > 17 && nation == "francais") {
+  //console.log("eligible")
+//} 
+//else {
+  //console.log("Pas eligible")
+//}
+//exercice
+//let age =Number(prompt("entrer age"));
+//if (age <= 5) {
+  //  result = 'gratuit';
+  //}
+  //else if (age > 5 && age <= 17 ) {
+    //result = 'réduit';
+  //} else{
+    //result = 'plein tarif';
+  //}
+  //console.log(result)
